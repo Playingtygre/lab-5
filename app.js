8a10f66440c2448bd2ec5b7a2d5bf5d6234f67b8
Playingtygre <kymiehsu123@gmail.com>
2017-08-11 18:13:33 -0500

    5th quetion

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ function sumArray(testArray){
 //   (arraySum);
   var sumArray = testArray + ' was passed in as an array of numbers, and ' + arraySum + ' is their sum.';
   console.log(sumArray);
-  return [arraySum, sumArray]; 
+  return [arraySum, sumArray];
 
 }
 
@@ -78,3 +78,22 @@ testSumArray(testArray);
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
+/////////////////////////////////////
+/* Problem 5
+Write a function called multiplyArray() that takes an array of numbers as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+"The numbers 2,3,4 have a product of 24."
+IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this.
+Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
+
+// Write your code here
+
+function multiplyArray(testArray){ //eslint-disable-line
+  var oneMultiply = multiply(multiply(testArray[0],testArray[1])[0],testArray[2])[0];
+  var ouput5 = 'The numbers ' + testArray + ' have a product of ' + oneMultiply + '.';
+  return [oneMultiply, ouput5];
+}
+
+// Here is the test for multiplyArray(); uncomment it to run it
+testMultiplyArray(2,3,4);
+
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
