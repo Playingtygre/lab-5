8f4b819a1c41a657cc8ebb4ff6d107a117b666ac
Playingtygre <kymiehsu123@gmail.com>
2017-08-11 15:14:09 -0500

    second question

--- app.js
+++ app.js
@@ -1,16 +1,5 @@
 'use strict';
 
-/////////////////////////////////////
-/* Problem 1 (this is your demo that we'll solve in class)
-Write a function called sum() that takes in two numbers as arguments and then returns an array where the first element is the sum of those numbers, and the second element is a concatenated string that EXACTLY follows this example and uses the parameters and values that were input/calculated in the function:
-
-"The sum of 4 and 7 is 11."
-
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.
-
-For TODO item, be sure to change it to say DONE when you have successfully completed the task*/
-
-// TODO: Write your code here
 function sum(a,b){ //eslint-disable-line
   var result = a + b;
   var output = 'The sum of ' + a + ' and ' + b + ' is ' + result + '.';
@@ -20,4 +9,19 @@ function sum(a,b){ //eslint-disable-line
 // TODO: Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
 
+/* Problem 2
+Write a function called multiply() that takes in two numbers as arguments and returns an array where the first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the parameters and values that were input/calculated into the function:
+"The product of 5 and 9 is 45."
+Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
+
+function multiply(a,b) {
+  var result = (a * b);
+  var output = 'The product of ' + a + ' and ' + b + ' is ' + result + '.';
+  return [result, output];
+}
+// TODO: Here is the test for multiply(); uncomment it to run it
+testMultiply(5,9);
+
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
+
+/////////////////////////////////////
